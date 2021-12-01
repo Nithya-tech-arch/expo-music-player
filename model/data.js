447f6b50e0ca2f635a9361529cedfe61592a16a3
Yuvaraj2111 <64225422+Yuvaraj2111@users.noreply.github.com>
2021-12-01 15:10:24 +0530

    Add play pause

--- model/data.js
+++ model/data.js
@@ -4,24 +4,28 @@ export const songs = [
         artist: "zayn",
         image: require("../assets/artwork/one_direction.jpeg"),
         id: "1",
+        url: require("../assets/songs/song1.mp3")
     },
     {
         title: "blank space",
         artist: "taylor",
         image: require("../assets/artwork/blank_space.jpeg"),
         id: "2",
+        url: require("../assets/songs/song2.mp3")
     },
     {
         title: "baby",
         artist: "justin",
         image: require("../assets/artwork/baby.jpeg"),
         id: "3",
+        url: require("../assets/songs/song3.mp3")
     },
     {
         title: "havana",
         artist: "camila",
         image: require("../assets/artwork/havana.jpeg"),
         id: "4",
+        url: require("../assets/songs/song4.mp3")
     },
 
 ];
