bd338e5b64cb448a57d34ea9c13e40e18c3e11d0
Yuvaraj2111 <64225422+Yuvaraj2111@users.noreply.github.com>
2021-12-17 12:54:55 +0530

    common components

---
+++ component/ErrorMessage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+
+const ErrorMessage = ({ error, visible }) => {
+    if (!error || !visible) {
+        return null;
+    }
+    return <Text style={styles.errorText}>⚠️ {error}</Text>;
+};
+
+const styles = StyleSheet.create({
+    errorText: {
+        color: 'red',
+        fontSize: 20,
+        marginBottom: 10,
+        fontWeight: '600'
+    }
+});
+
+export default ErrorMessage;
